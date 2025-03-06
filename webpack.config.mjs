851431394f35976b99ba484ee2e2535a21c06dbc
Deyang Dai <deyangda@amazon.com>
2025-03-05 16:45:28 -0800

    docs: add sandbox to GitHub pages

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -1,5 +1,4 @@
-import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
-import CompressionPlugin from 'compression-webpack-plugin';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import { fileURLToPath } from 'url';
 import path from 'path';
@@ -7,35 +6,38 @@ import path from 'path';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default (env, argv) => {
-    const isProd = env.production;
+    const isProd = env.production || false;
 
     const plugins = [
         new HtmlWebpackPlugin({
             template: './src/dev/viewer.html',
-            filename: (entryName) => `${entryName}.html`,
+            filename: 'index.html',
+            chunks: ['index'],
         }),
-        new CompressionPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/dev/viewer.html',
+            filename: 'v3dViewer.html',
+            chunks: ['v3dViewer'],
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'public', to: 'public' },
+                { from: 'docs', to: 'docs', noErrorOnMissing: true },
+            ],
+        }),
     ];
-
-    if (isProd) {
-        plugins.push(
-            new BundleAnalyzerPlugin({
-                analyzerMode: 'static',
-            }),
-        );
-    }
 
     return {
         mode: isProd ? 'production' : 'development',
         devtool: isProd ? false : 'eval-cheap-module-source-map',
         entry: {
-            index: './src/index.ts',
-            viewerIndex: './src/dev/index.ts',
+            index: './src/dev/index.ts',
             v3dViewer: './src/dev/v3dViewer.ts',
         },
         output: {
-            path: path.resolve(__dirname, 'dist'),
-            filename: '[name].js',
+            path: path.resolve(__dirname, 'web'),
+            filename: '[name].bundle.js',
+            clean: true,
         },
         resolve: {
             extensions: ['.tsx', '.js', '.ts'],
@@ -57,12 +59,15 @@ export default (env, argv) => {
             ],
         },
         devServer: {
-            static: {
+            static: [{
                 directory: path.resolve(__dirname, 'public'),
                 publicPath: '/public',
-            },
+            }, {
+                directory: path.resolve(__dirname, 'docs'),
+                publicPath: '/docs',
+            }],
             compress: false,
-            open: 'viewerIndex',
+            open: 'index.html',
             port: 8443,
             server: 'http',
             hot: true,
