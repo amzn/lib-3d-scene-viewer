a194806f654a458ace6257bb38494241b4d44c57
Deyang Dai <deyangda@amazon.com>
2025-07-29 13:23:16 -0700

    ci: set the body-max-line-length rule in commitlint as a warning rather than an error

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
-        'body-max-line-length': [2, 'always', 140],
+        'body-max-line-length': [1, 'always', 140],
     },
 };
